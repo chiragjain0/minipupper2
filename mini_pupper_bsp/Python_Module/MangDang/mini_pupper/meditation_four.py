9c968c9248b86a87715cb2128f50bf185b8be7b1
chiragjain0 <jainch@oregonstate.edu>
2024-04-08 13:12:02 -0700

    while loop

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/meditation_four.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/meditation_four.py
@@ -113,178 +113,179 @@ if __name__ == "__main__":
     # Your additional code logic can go here.
     pygame.event.pump()
 
-    msg = {
-        "ly": 0,
-        "lx": 0,
-        "rx": 0,
-        "ry": 0,
-        "L2": 0,
-        "R2": 0,
-        "R1": 0,
-        "L1": 0,
-        "dpady": 0,
-        "dpadx": 0,
-        "x": 0,
-        "square": 0,
-        "circle": 0,
-        "triangle": 0,
-        "message_rate": MESSAGE_RATE,
-    }
-    pub.send(msg)
-    #pygame.display.flip()
-    pygame.time.wait(int(1000/MESSAGE_RATE))
+    # msg = {
+    #     "ly": 0,
+    #     "lx": 0,
+    #     "rx": 0,
+    #     "ry": 0,
+    #     "L2": 0,
+    #     "R2": 0,
+    #     "R1": 0,
+    #     "L1": 0,
+    #     "dpady": 0,
+    #     "dpadx": 0,
+    #     "x": 0,
+    #     "square": 0,
+    #     "circle": 0,
+    #     "triangle": 0,
+    #     "message_rate": MESSAGE_RATE,
+    # }
+    # pub.send(msg)
+    # #pygame.display.flip()
+    # pygame.time.wait(int(1000/MESSAGE_RATE))
+    # pygame.time.wait(500)
+    # #if not pygame.key.get_focused():
+    #     #print("Application not in focus! Click the application window to re-enable control.")
+    # #else:
+    # msg = {
+    #         "ly": 0,
+    #         "lx": 0,
+    #         "rx": 0,
+    #         "ry": 0,
+    #         "L2": 0,
+    #         "R2": 0,
+    #         "R1": 0,
+    #         "L1": 1,
+    #         "dpady": 0,
+    #         "dpadx": 0,
+    #         "x": 0,
+    #         "square": 0,
+    #         "circle": 0,
+    #         "triangle": 0,
+    #         "message_rate": MESSAGE_RATE,
+    #     }
+    # pub.send(msg)
+    # #pygame.display.flip()
+    # pygame.time.wait(int(1000/MESSAGE_RATE))
+    # pygame.time.wait(500)
+    # msg = {
+    #         "ly": 0,
+    #         "lx": 0,
+    #         "rx": 0,
+    #         "ry": 0,
+    #         "L2": 0,
+    #         "R2": 0,
+    #         "R1": 0,
+    #         "L1": 0,
+    #         "dpady": 0,
+    #         "dpadx": 0,
+    #         "x": 0,
+    #         "square": 0,
+    #         "circle": 0,
+    #         "triangle": 0,
+    #         "message_rate": MESSAGE_RATE,
+    #     }
+    # pub.send(msg)
+    # #pygame.display.flip()
+    # pygame.time.wait(int(500/MESSAGE_RATE))
     pygame.time.wait(500)
-    #if not pygame.key.get_focused():
-        #print("Application not in focus! Click the application window to re-enable control.")
-    #else:
-    msg = {
-            "ly": 0,
-            "lx": 0,
-            "rx": 0,
-            "ry": 0,
-            "L2": 0,
-            "R2": 0,
-            "R1": 0,
-            "L1": 1,
-            "dpady": 0,
-            "dpadx": 0,
-            "x": 0,
-            "square": 0,
-            "circle": 0,
-            "triangle": 0,
-            "message_rate": MESSAGE_RATE,
-        }
-    pub.send(msg)
-    #pygame.display.flip()
-    pygame.time.wait(int(1000/MESSAGE_RATE))
-    pygame.time.wait(500)
-    msg = {
-            "ly": 0,
-            "lx": 0,
-            "rx": 0,
-            "ry": 0,
-            "L2": 0,
-            "R2": 0,
-            "R1": 0,
-            "L1": 0,
-            "dpady": 0,
-            "dpadx": 0,
-            "x": 0,
-            "square": 0,
-            "circle": 0,
-            "triangle": 0,
-            "message_rate": MESSAGE_RATE,
-        }
-    pub.send(msg)
-    #pygame.display.flip()
-    pygame.time.wait(int(500/MESSAGE_RATE))
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_00_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_01_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_02_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_03_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_04_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_05_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_06_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_07_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_08_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden2/frame_09_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_00_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_01_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_00_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_01_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_02_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_03_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_04_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_05_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_06_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_07_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_08_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_09_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_10_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_11_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_12_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_13_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_14_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_15_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_16_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_17_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_18_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_19_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_20_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_21_delay-0.1s.png')
-    pygame.time.wait(500)   
-    disp.show_image('owlpng/garden3/frame_22_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_23_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_24_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_25_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_26_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_27_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_28_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_29_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_30_delay-0.1s.png')
-    pygame.time.wait(500)
-    disp.show_image('owlpng/garden3/frame_31_delay-0.1s.png')
-    pygame.time.wait(500)
-
-    msg = {
-            "ly": 0,
-            "lx": 0,
-            "rx": 0,
-            "ry": 0,
-            "L2": 0,
-            "R2": 0,
-            "R1": 0,
-            "L1": 1,
-            "dpady": 0,
-            "dpadx": 0,
-            "x": 0,
-            "square": 0,
-            "circle": 0,
-            "triangle": 0,
-            "message_rate": MESSAGE_RATE,
-        }
-    pub.send(msg)
-    #pygame.display.flip()
-    pygame.time.wait(int(1000/MESSAGE_RATE))
+    while(1):
+        disp.show_image('owlpng/garden2/frame_00_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_01_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_02_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_03_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_04_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_05_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_06_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_07_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_08_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden2/frame_09_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_00_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_01_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_00_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_01_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_02_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_03_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_04_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_05_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_06_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_07_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_08_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_09_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_10_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_11_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_12_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_13_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_14_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_15_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_16_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_17_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_18_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_19_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_20_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_21_delay-0.1s.png')
+        pygame.time.wait(500)   
+        disp.show_image('owlpng/garden3/frame_22_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_23_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_24_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_25_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_26_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_27_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_28_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_29_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_30_delay-0.1s.png')
+        pygame.time.wait(500)
+        disp.show_image('owlpng/garden3/frame_31_delay-0.1s.png')
+        pygame.time.wait(500)
+    
+        # msg = {
+        #         "ly": 0,
+        #         "lx": 0,
+        #         "rx": 0,
+        #         "ry": 0,
+        #         "L2": 0,
+        #         "R2": 0,
+        #         "R1": 0,
+        #         "L1": 1,
+        #         "dpady": 0,
+        #         "dpadx": 0,
+        #         "x": 0,
+        #         "square": 0,
+        #         "circle": 0,
+        #         "triangle": 0,
+        #         "message_rate": MESSAGE_RATE,
+        #     }
+    # pub.send(msg)
+    # #pygame.display.flip()
+    # pygame.time.wait(int(1000/MESSAGE_RATE))

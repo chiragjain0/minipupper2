f38cafe21a4d7053885e670fa76fc99edbb2b1a1
chiragjain0 <jainch@oregonstate.edu>
2024-04-28 16:36:17 -0700

    audio files

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/slot_machine.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/slot_machine.py
@@ -63,6 +63,17 @@ if __name__ == "__main__":
     msg = movement_rx_ry(0.3,0.3)
     pub_msg(msg,wait_time)
 
+    msg = movement_rx_ry(0.3,0.3)
+    pub_msg(msg,wait_time)
+
+    msg = movement_rx_ry(0.3,0.3)
+    pub_msg(msg,wait_time)
+
+    msg = movement_rx_ry(0.3,0.3)
+    pub_msg(msg,wait_time)
+
+    msg = movement_rx_ry(0.3,0.3)
+    pub_msg(msg,wait_time)
 
     msg,counter_m = toggle_movement(counter_m)
     pub_msg(msg,wait_time)

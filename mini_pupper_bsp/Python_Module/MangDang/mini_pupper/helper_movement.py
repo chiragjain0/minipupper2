ff5eecee6ff1d3cd7f67883ba20d7fad97608f59
chiragjain0 <jainch@oregonstate.edu>
2024-04-28 13:43:13 -0700

    meditationa nd helper script

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/helper_movement.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/helper_movement.py
@@ -1,3 +1,5 @@
+MESSAGE_RATE = 20
+
 def reset():
     msg = {
         "ly": 0,
@@ -18,7 +20,7 @@ def reset():
     }
     return msg
 
-def movement_L1():
+def toggle_activation():
         msg = {
             "ly": 0,
             "lx": 0,
@@ -38,7 +40,27 @@ def movement_L1():
         }
         return msg
 
-def movement_dpapx():
+def toggle_movement():
+        msg = {
+            "ly": 0,
+            "lx": 0,
+            "rx": 0,
+            "ry": 0,
+            "L2": 0,
+            "R2": 0,
+            "R1": 1,
+            "L1": 0,
+            "dpady": 0,
+            "dpadx": 0,
+            "x": 0,
+            "square": 0,
+            "circle": 0,
+            "triangle": 0,
+            "message_rate": MESSAGE_RATE,
+        }
+        return msg
+
+def movement_dpapx_negative():
     msg = {
         "ly": 0,
         "lx": 0,
@@ -58,7 +80,27 @@ def movement_dpapx():
     }
     return msg
 
-def movement_dpapy():
+def movement_dpapx_positive():
+    msg = {
+        "ly": 0,
+        "lx": 0,
+        "rx": 0,
+        "ry": 0,
+        "L2": 0,
+        "R2": 0,
+        "R1": 0,
+        "L1": 0,
+        "dpady": 0,
+        "dpadx": 1,
+        "x": 0,
+        "square": 0,
+        "circle": 0,
+        "triangle": 0,
+        "message_rate": MESSAGE_RATE,
+    }
+    return msg
+
+def movement_dpapy_positive():
     msg = {
         "ly": 0,
         "lx": 0,
@@ -76,11 +118,31 @@ def movement_dpapy():
         "triangle": 0,
         "message_rate": MESSAGE_RATE,
     }
-def movement_rx():  
+
+def movement_dpapy_negative():
     msg = {
         "ly": 0,
         "lx": 0,
-        "rx": 0.3,#counter
+        "rx": 0,#counter
+        "ry": 0,#clockwise
+        "L2": 0,
+        "R2": 0,
+        "R1": 0,
+        "L1": 0,
+        "dpady": -1,
+        "dpadx": 0,
+        "x": 0,
+        "square": 0,
+        "circle": 0,
+        "triangle": 0,
+        "message_rate": MESSAGE_RATE,
+    }
+
+def movement_rx(val):  
+    msg = {
+        "ly": 0,
+        "lx": 0,
+        "rx": val,#counter
         "ry": 0,#clockwise
         "L2": 0,
         "R2": 0,
@@ -96,12 +158,52 @@ def movement_rx():
     }
     return msg
 
-def movement_ry():
+def movement_ry(val):
     msg = {
         "ly": 0,
         "lx": 0,
         "rx": 0,#counter
-        "ry": 0.3,#clockwise
+        "ry": val,#clockwise
+        "L2": 0,
+        "R2": 0,
+        "R1": 0,
+        "L1": 0,
+        "dpady": 0,
+        "dpadx": 0,
+        "x": 0,
+        "square": 0,
+        "circle": 0,
+        "triangle": 0,
+        "message_rate": MESSAGE_RATE,
+    }
+    return msg
+
+def movement_lx(val):  
+    msg = {
+        "ly": 0,
+        "lx": val,
+        "rx": 0,#counter
+        "ry": 0,#clockwise
+        "L2": 0,
+        "R2": 0,
+        "R1": 0,
+        "L1": 0,
+        "dpady": 0,
+        "dpadx": 0,
+        "x": 0,
+        "square": 0,
+        "circle": 0,
+        "triangle": 0,
+        "message_rate": MESSAGE_RATE,
+    }
+    return msg
+
+def movement_ly(val):  
+    msg = {
+        "ly": val,
+        "lx": 0,
+        "rx": 0,#counter
+        "ry": 0,#clockwise
         "L2": 0,
         "R2": 0,
         "R1": 0,

bfbed6d2c0e3dd29bb5b3e7fb8545fd3fd097ef7
chiragjain0 <jainch@oregonstate.edu>
2024-05-17 16:37:02 -0700

    esp

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
@@ -1,19 +1,23 @@
 import HardwareInterface
 from MangDang.mini_pupper.Config import ServoParams, PWMParams
 import numpy as np
+from MangDang.mini_pupper.ESP32Interface import ESP32Interface
 
-joint = np.zeros((3, 4))  # Assuming 3 axes and 4 legs
+positions = [512, 512, 0, 512, 512, 512, 512, 512, 512, 512, 512, 512]
+esp32 = ESP32Interface()
+esp32.servos_set_position(positions)
+# joint = np.zeros((3, 4))  # Assuming 3 axes and 4 legs
 
-angle = 10
-servo_params = ServoParams()
-axis_index = 1
-leg_index = 2
+# angle = 10
+# servo_params = ServoParams()
+# axis_index = 1
+# leg_index = 2
 
-HardwareInterface.angle_to_position(angle, servo_params, axis_index, leg_index)
+# HardwareInterface.angle_to_position(angle, servo_params, axis_index, leg_index)
 
-pwm_params = PWMParams()
+# pwm_params = PWMParams()
 
-joint[axis_index, leg_index] = angle
+# joint[axis_index, leg_index] = angle
 
-joint_angles = joint[axis_index, leg_index]
-HardwareInterface.send_servo_commands(pwm_params, servo_params, joint_angles)
+# joint_angles = joint[axis_index, leg_index]
+# HardwareInterface.send_servo_commands(pwm_params, servo_params, joint_angles)

90b9905bef7b8f704bcb86763a32d6842f39eb17
Yuxuan He <heyux@oregonstate.edu>
2024-05-17 16:19:42 -0700

    modified

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
@@ -1,6 +1,8 @@
 import HardwareInterface
 from MangDang.mini_pupper.Config import ServoParams, PWMParams
+import numpy as np
 
+joint = np.zeros((3, 4))  # Assuming 3 axes and 4 legs
 
 angle = 10
 servo_params = ServoParams()
@@ -11,7 +13,7 @@ HardwareInterface.angle_to_position(angle, servo_params, axis_index, leg_index)
 
 pwm_params = PWMParams()
 
-joint = []
+joint[axis_index, leg_index] = angle
 
-joint_angles = joint[axis_index,leg_index]
+joint_angles = joint[axis_index, leg_index]
 HardwareInterface.send_servo_commands(pwm_params, servo_params, joint_angles)

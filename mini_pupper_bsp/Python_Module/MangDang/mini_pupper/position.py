591a0ac28efd68110654d3f78540939d3d58b746
Yuxuan He <heyux@oregonstate.edu>
2024-05-23 15:04:46 -0700

    .

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
@@ -1,6 +1,6 @@
 from MangDang.mini_pupper.ESP32Interface import ESP32Interface
 
-positions = [512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]
+positions = [100, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]
 esp32 = ESP32Interface()
 #positions = [512, 512, 100, 512, 100, 512, 512, 512, 512, 512, 512, 512]
 

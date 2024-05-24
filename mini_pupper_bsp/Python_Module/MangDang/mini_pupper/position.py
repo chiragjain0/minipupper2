e246a797b832bbf0d59308b14986fa2fd3e89806
chiragjain0 <jainch@oregonstate.edu>
2024-05-24 15:21:03 -0700

    blah

--- mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/position.py
@@ -5,20 +5,3 @@ esp32 = ESP32Interface()
 #positions = [512, 512, 100, 512, 100, 512, 512, 512, 512, 512, 512, 512]
 
 esp32.servos_set_position(positions)
-
-
-# joint = np.zeros((3, 4))  # Assuming 3 axes and 4 legs
-
-# angle = 10
-# servo_params = ServoParams()
-# axis_index = 1
-# leg_index = 2
-
-# HardwareInterface.angle_to_position(angle, servo_params, axis_index, leg_index)
-
-# pwm_params = PWMParams()
-
-# joint[axis_index, leg_index] = angle
-
-# joint_angles = joint[axis_index, leg_index]
-# HardwareInterface.send_servo_commands(pwm_params, servo_params, joint_angles)
---
+++ mini_pupper_bsp/Python_Module/MangDang/mini_pupper/servo_interface_cj.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from MangDang.mini_pupper.HardwareInterface import HardwareInterface
+
+hardware_interface = HardwareInterface()
+
+joint_positions = [100, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512, 512]
+
+lf1_position = joint_positions[0]
+lf2_position = joint_positions[1]
+lf3_position = joint_positions[2]
+rf1_position = joint_positions[3]
+rf2_position = joint_positions[4]
+rf3_position = joint_positions[5]
+lb1_position = joint_positions[6]
+lb2_position = joint_positions[7]
+lb3_position = joint_positions[8]
+rb1_position = joint_positions[9]
+rb2_position = joint_positions[10]
+rb3_position = joint_positions[11]
+
+joint_angles = np.array([
+            [rf1_position, lf1_position, rb1_position, lb1_position],
+            [rf2_position, lf2_position, rb2_position, lb2_position],
+            [rf2_position + rf3_position, lf2_position + lf3_position,
+             rb2_position + rb3_position, lb2_position + lb3_position]
+        ])
+
+hardware_interface.set_actuator_postions(joint_angles)
+
